9c4c536e81810203d5a756e5f05572a38222f39c
Daly <dalyna.pak@hotmail.fr>
2019-12-21 20:53:44 -0500

    Add logo and bouncing logic
    Signed-off-by: Daly <dalyna.pak@hotmail.fr>

---
+++ bouncing.py
@@ -0,0 +1,38 @@
+import pygame
+import sys
+
+pygame.init()
+
+speed = [8, 7]
+size = width, height = 600, 600
+screen = pygame.display.set_mode(size)
+clock = pygame.time.clock
+logo = pygame.image.load("./DVD/dvdgreen.png")
+rect = logo.get_rect()
+fps = 60
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    
+    if rect.left < 0:
+        speed[0] = -speed[0]
+        logo = pygame.image.load("./DVD/dvdblue.png")
+    if rect.right > width:
+        speed[0] = -speed[0]
+        logo = pygame.image.load("./DVD/dvdred.png")
+
+    if rect.top < 0:
+        speed[1] = -speed[1]
+        logo = pygame.image.load("./DVD/dvdgreen.png")
+
+    if rect.bottom > height:
+        speed[1] = -speed[1]
+        logo = pygame.image.load("./DVD/dvdyellow.png")
+
+    rect.left += speed[0]
+    rect.top += speed[1]
+    screen.fill((0, 0 , 0))
+    screen.blit(logo, rect)
+    pygame.display.update()
+    clock.tick(fps)
